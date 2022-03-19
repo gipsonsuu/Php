ce3fc5e4d930333b67acee6aeb7ca462f1ba74b3
Gipson Suu <91767873+gipsonsuu@users.noreply.github.com>
2022-03-19 09:53:41 +0900

    foreach - php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foreach - Gipson suu</title>
+</head>
+<body>
+    
+<?php 
+//Loop
+
+//while Loop
+//Do while Loop
+//For Loop
+//Foreach Loop
+$array = array("Daniel", "Jane", "Jacob", "Jhon", "Mariane");
+foreach ($array as $loopdata) {
+echo " My name is ".$loopdata."<br>";
+}
+
+?>
+
+
+
+</body>
+</html>
+
+
+
+
+
